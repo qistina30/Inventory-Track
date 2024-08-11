8c91abaf3486d8379370331a597b3d07c32baeec
qistina <naqistina00@gmail.com>
2024-08-11 13:39:22 +0800

    first commit

---
+++ database/migrations/2024_08_11_044502_create_locations_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id(); // unsignedBigInt by default
+            $table->integer('rack');
+            $table->integer('row');
+            $table->integer('column');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('locations');
+    }
+};
